2b20152ed2f3dd120f314d39662544f4590c3eb2
Jared Sutch <sutch002@gmail.com>
2021-09-01 12:14:28 -0500

    trying to fix a bug

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ const renderCalender = () => {
 
   document.querySelector(".date h1").innerHTML = months[date.getMonth()];
 
-  document.querySelector(".date p").innerHTML = date.toDateString();
+  document.querySelector(".date p").innerHTML = new Date().toDateString();
 
   let days = "";
 
